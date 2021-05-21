874a6e33d122ef67b72ba0fa7c1f9bfa52509e3c
Schneider Shades <schneidershades@gmail.com>
2021-05-21 03:28:42 +0100

    annotation

--- app/Http/Controllers/Api/Country/CountryController.php
+++ app/Http/Controllers/Api/Country/CountryController.php
@@ -8,6 +8,23 @@ use App\Models\Country;
 
 class CountryController extends Controller
 {
+	/**
+    * @OA\Get(
+    *      path="/api/v1/location/countries",
+    *      operationId="all_countries",
+    *      tags={"location"},
+    *      summary="Get all countries",
+    *      description="Get all countries",
+    *      @OA\Response(
+    *          response=200,
+    *          description="Successful operation",
+    *          @OA\MediaType(
+    *             mediaType="application/json",
+    *         ),
+    *       ),
+    * )
+    */
+
     public function index ()
     {
     	return Country::all();
