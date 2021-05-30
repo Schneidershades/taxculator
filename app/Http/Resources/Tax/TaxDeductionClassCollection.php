5bb15ea46ca3037473d8609ab1114ed2871c69c0
Schneider Shades <schneidershades@gmail.com>
2021-05-30 19:53:40 +0100

    naming

--- app/Http/Resources/Tax/TaxDeductionClassCollection.php
+++ app/Http/Resources/Tax/TaxDeductionClassCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Tax;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class TaxDeductionCollection extends ResourceCollection
+class TaxDeductionClassCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
--- app/Http/Resources/Tax/TaxDeductionClassResource.php
+++ app/Http/Resources/Tax/TaxDeductionClassResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Tax;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TaxDeductionResource extends JsonResource
+class TaxDeductionClassResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
