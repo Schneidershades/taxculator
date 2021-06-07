9d9b65f71ea9a1e4f2bb2a4e261dbcc51e1895a0
Schneider Shades <schneidershades@gmail.com>
2021-06-07 02:45:21 +0100

    push correction

--- app/Http/Resources/Tax/TaxTransactionRelationResource.php
+++ app/Http/Resources/Tax/TaxTransactionRelationResource.php
@@ -3,6 +3,9 @@
 namespace App\Http\Resources\Tax;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Tax\TaxClassResource;
+use App\Http\Resources\Country\CountryTaxDeductionClassResource;
+use App\Http\Resources\Country\CountryTaxReliefClassResource;
 
 class TaxTransactionRelationResource extends JsonResource
 {
@@ -19,7 +22,18 @@ class TaxTransactionRelationResource extends JsonResource
             'description' => $this->description,
             'value' => $this->value,
             'applied_by' => $this->applied_by,
-            'category' => $this->taxTransactionRelationable ? $this->taxTransactionRelationable : null,
+            $this->mergeWhen($this->description == 'taxClass', [
+                'category' => new TaxClassResource($this->taxTransactionRelationable),
+            ]),
+            $this->mergeWhen($this->description == 'countryTaxDeductionClass', [
+                'category' => new CountryTaxDeductionClassResource($this->taxTransactionRelationable),
+            ]),
+            $this->mergeWhen($this->description == 'countryTaxReliefClass', [
+                'category' => new CountryTaxReliefClassResource($this->taxTransactionRelationable),
+            ]),
+            $this->mergeWhen($this->description == null , [
+                'category' => $this->taxTransactionRelationable ? $this->taxTransactionRelationable : null,
+            ]),
         ];
     }
 }
