67812ccecfc01172b7b6065fdabb4ce5eb359e03
Schneider <33626602+Schneidershades@users.noreply.github.com>
2025-08-19 18:13:35 +0100

    update 2025

--- app/Http/Controllers/Api/Tax/TaxTransactionController.php
+++ app/Http/Controllers/Api/Tax/TaxTransactionController.php
@@ -14,41 +14,9 @@ class TaxTransactionController extends Controller
     {
         $this->service = $service;
     }
-    /**
-    * @OA\Post(
-    *      path="/api/v1/tax-transactions",
-    *      operationId="taxTransactions",
-    *      tags={"company"},
-    *      summary="Post annual tax of an employee",
-    *      description="Post annual tax of an employee",
-    *      @OA\RequestBody(
-    *          required=true,
-    *          @OA\JsonContent(ref="#/components/schemas/TaxTransactionCreateFormRequest")
-    *      ),
-    *      @OA\Response(
-    *          response=200,
-    *          description="Successful signin",
-    *          @OA\MediaType(
-    *             mediaType="application/json",
-    *         ),
-    *       ),
-    *      @OA\Response(
-    *          response=400,
-    *          description="Bad Request"
-    *      ),
-    *      @OA\Response(
-    *          response=401,
-    *          description="unauthenticated",
-    *      ),
-    *      @OA\Response(
-    *          response=403,
-    *          description="Forbidden"
-    *      ),
-    *      security={ {"bearerAuth": {}} },
-    * )
-    */
-    public function store (TaxTransactionCreateFormRequest $request)
+
+    public function store(TaxTransactionCreateFormRequest $request)
     {
-    	return $this->showOne($this->service->register($request));
+        return $this->showOne($this->service->register($request));
     }
 }
--- app/Services/Tax/TaxTransactionService.php
+++ app/Services/Tax/TaxTransactionService.php
@@ -2,8 +2,6 @@
 
 namespace App\Services\Tax;
 
-use App\Services\Tax\TaxTransactionClass;
-use App\Http\Requests\Tax\TaxTransactionCreateFormRequest;
 use App\Models\TaxClass;
 use App\Models\TaxTransaction;
 use App\Models\TaxTransactionRelation;
@@ -15,88 +13,86 @@ use App\Models\CountryTaxTarrif;
 
 class TaxTransactionService
 {
-	public function register($request)
+    public function register($request)
     {
         // $taxTransaction = TaxTransaction::where('id', 1)->first();
 
-    	$taxTransaction = new TaxTransaction;
-    	$taxTransaction->save();
-    	foreach ($request['taxClasses'][0] as $key => $value) {
-    		if($value){
-    			$taxClass = TaxClass::where('short_name', $key)->first();
+        $taxTransaction = new TaxTransaction;
+        $taxTransaction->save();
+        foreach ($request['taxClasses'][0] as $key => $value) {
+            if ($value) {
+                $taxClass = TaxClass::where('short_name', $key)->first();
                 $this->newTransactionRelative($taxTransaction->id, 'taxClass', $value, 'amount', $taxClass->id, 'taxClass');
-    		}
-    	}
+            }
+        }
 
-    	foreach ($request['taxDeductions'][0] as $key => $value) {
+        foreach ($request['taxDeductions'][0] as $key => $value) {
 
-    		$taxDeduction = TaxDeductionClass::where('short_name', $key)->first();
+            $taxDeduction = TaxDeductionClass::where('short_name', $key)->first();
 
-    		$countryTaxDeductionClass = CountryTaxDeductionClass::where('tax_deduction_class_id', $taxDeduction->id)->first();
+            $countryTaxDeductionClass = CountryTaxDeductionClass::where('tax_deduction_class_id', $taxDeduction->id)->first();
 
-    		if($countryTaxDeductionClass->deduction_type == 'amount'){
-    			$nhf = $countryTaxDeductionClass->value;
-    		}
+            if ($countryTaxDeductionClass->deduction_type == 'amount') {
+                $nhf = $countryTaxDeductionClass->value;
+            }
 
-    		if($countryTaxDeductionClass->deduction_type == 'percentage'){
-    			$divide = $countryTaxDeductionClass->value / 100;
+            if ($countryTaxDeductionClass->deduction_type == 'percentage') {
+                $divide = $countryTaxDeductionClass->value / 100;
 
-    			$countryClassDeduction = CountryClassDeduction::where('country_tax_deduction_class_id', $countryTaxDeductionClass->id)
-    										->pluck('country_tax_class_id')
-    										->toArray();
+                $countryClassDeduction = CountryClassDeduction::where('country_tax_deduction_class_id', $countryTaxDeductionClass->id)
+                    ->pluck('country_tax_class_id')
+                    ->toArray();
 
-    			$deductClasses = TaxClass::whereIn('id', $countryClassDeduction)
-    								->pluck('short_name')
-    								->toArray();
+                $deductClasses = TaxClass::whereIn('id', $countryClassDeduction)
+                    ->pluck('short_name')
+                    ->toArray();
 
-    			$items = array_intersect_key( $request['taxClasses'][0], array_flip($deductClasses));
+                $items = array_intersect_key($request['taxClasses'][0], array_flip($deductClasses));
 
-    			$sum = array_sum($items) * $divide;
+                $sum = array_sum($items) * $divide;
 
                 $this->newTransactionRelative($taxTransaction->id, 'countryTaxDeductionClass', $sum, 'percentage', $countryTaxDeductionClass->id, 'countryTaxDeductionClass');
+            }
+        }
 
-    		}
-    	}
-
-    	$grossIncome = (int)$taxTransaction->taxTranactionRelatives->sum('value');
+        $grossIncome = (int)$taxTransaction->taxTranactionRelatives->sum('value');
 
         $this->newTransactionRelative($taxTransaction->id, 'grossIncome', $grossIncome, 'amount', null, null);
 
-    	$reliefs = CountryTaxReliefClass::where('country_id', $request['country_id'])->get();
+        $reliefs = CountryTaxReliefClass::where('country_id', $request['country_id'])->get();
 
-    	$reliefAmount = 0;
+        $reliefAmount = 0;
 
-    	foreach ($reliefs as $relief) {
+        foreach ($reliefs as $relief) {
 
             $applied_by = $relief['relief_type'];
             $model_id = $relief->id;
             $model_type = 'countryTaxReliefClass';
 
-            if($relief['minimum_status'] == 'unlimited' && $relief['maximum_status'] == 'unlimited' ){
+            if ($relief['minimum_status'] == 'unlimited' && $relief['maximum_status'] == 'unlimited') {
                 $value = $grossIncome * $relief->value / 100;
                 $reliefAmount += $value;
                 $this->newTransactionRelative($taxTransaction->id, $model_type, $value, $applied_by, $model_id, $model_type);
             }
 
-            if($grossIncome >= $relief['minimum_amount'] && $relief['minimum_status'] == 'static' && $relief['maximum_status'] == 'unlimited' ){
+            if ($grossIncome >= $relief['minimum_amount'] && $relief['minimum_status'] == 'static' && $relief['maximum_status'] == 'unlimited') {
                 $value = $grossIncome * $relief->value / 100;
                 $reliefAmount += $value;
                 $this->newTransactionRelative($taxTransaction->id, $model_type, $value, $applied_by, $model_id, $model_type);
             }
 
-    		if($grossIncome > $relief['minimum_amount'] && $grossIncome > $relief['maximum_amount'] && $relief->taxReliefClass->code == 'fixed'){
+            if ($grossIncome > $relief['minimum_amount'] && $grossIncome > $relief['maximum_amount'] && $relief->taxReliefClass->code == 'fixed') {
                 $value = $grossIncome * $relief->value / 100;
                 $reliefAmount += $value;
                 $this->newTransactionRelative($taxTransaction->id, $model_type, $value, $applied_by, $model_id, $model_type);
-
-    		}elseif($grossIncome > $relief['minimum_amount'] && $grossIncome < $relief['maximum_amount'] && $relief['maximum_status'] == 'static' && $relief['maximum_status'] == 'static'){
+            } elseif ($grossIncome > $relief['minimum_amount'] && $grossIncome < $relief['maximum_amount'] && $relief['maximum_status'] == 'static' && $relief['maximum_status'] == 'static') {
                 $value = $relief->value;
                 $reliefAmount += $relief->value;
                 $this->newTransactionRelative($taxTransaction->id, $model_type, $value, $applied_by, $model_id, $model_type);
-    		}
-    	}
+            }
+        }
 
-    	$taxableIncome = $grossIncome - $reliefAmount;
+        $taxableIncome = $grossIncome - $reliefAmount;
 
         $this->newTransactionRelative($taxTransaction->id, 'taxableIncome', $taxableIncome, 'amount', null, null);
 
@@ -105,10 +101,9 @@ class TaxTransactionService
         $this->processTaxTarrif($taxTransaction, $taxableIncome, $tarrifsOrderedByPercentage);
 
         return $taxTransaction;
-
     }
 
-    private function newTransactionRelative ($id, $description, $value, $applied_by, $model_id=null, $model_type=null)
+    private function newTransactionRelative($id, $description, $value, $applied_by, $model_id = null, $model_type = null)
     {
         $taxRelation = new TaxTransactionRelation;
         $taxRelation->tax_transaction_id = $id;
@@ -121,28 +116,29 @@ class TaxTransactionService
     }
 
 
-    private function processTaxTarrif($taxTransaction, $amount, $tarrifsOrderedByPercentage, $tarrifsCollective = null){
+    private function processTaxTarrif($taxTransaction, $amount, $tarrifsOrderedByPercentage, $tarrifsCollective = null)
+    {
 
         $tarrifsCollective = [];
         $taxCollective = [];
 
         $remainingAmount = $amount;
 
-        if($remainingAmount >= 0){
+        if ($remainingAmount >= 0) {
 
-            foreach($tarrifsOrderedByPercentage as $tarrif){
+            foreach ($tarrifsOrderedByPercentage as $tarrif) {
 
-                if(!in_array($tarrif['ordering_id'], $tarrifsCollective)){
+                if (!in_array($tarrif['ordering_id'], $tarrifsCollective)) {
 
                     $amountRendered = $remainingAmount - $tarrif['fixed_amount'];
 
-                    if($amountRendered <= 0 || $tarrif['fixed_amount'] == '0.00'){
-                        $amountByPercentage = $remainingAmount * $tarrif['fixed_percentage']/100;
-                    }elseif($amountRendered > 0 ){
-                        $amountByPercentage = $tarrif['fixed_amount'] * $tarrif['fixed_percentage']/100;
+                    if ($amountRendered <= 0 || $tarrif['fixed_amount'] == '0.00') {
+                        $amountByPercentage = $remainingAmount * $tarrif['fixed_percentage'] / 100;
+                    } elseif ($amountRendered > 0) {
+                        $amountByPercentage = $tarrif['fixed_amount'] * $tarrif['fixed_percentage'] / 100;
                     }
 
-                    if ($remainingAmount > 0){
+                    if ($remainingAmount > 0) {
                         $tarrifsCollective[] = $tarrif['ordering_id'];
                         $taxCollective[] = $amountByPercentage;
 
